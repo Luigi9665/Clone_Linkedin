b1d4a188f3a1aeabd28ae5adff08d40e738ec9a7
MREBECCA98 <reb.matarozzo@gmail.com>
2025-10-22 14:57:48 +0200

    ColCenterHomePage

--- src/Pages/MyHome.jsx
+++ src/Pages/MyHome.jsx
@@ -1,6 +1,9 @@
+import ColCenterHomePage from "../components/ColCenterHomePage";
+
 const MyHome = () => {
   return (
     <div>
+      <ColCenterHomePage />
       <p>1</p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, minima. Totam sunt ipsum praesentium dolores eveniet harum quasi possimus nostrum
